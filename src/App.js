a600ddfd4a6d5b73a5058954dfd5191e478f77b0
cjkriska <charleskriska@gmail.com>
2022-09-03 23:03:55 -0500

    add Main, Navbar, and Map components with galaxy image

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
+import Main from "./components/Main";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello there!
-    </h1>
+    <Main />
   );
 }
 
---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import Map from "./Map";
+
+function Main() {
+    return (
+        <>
+            <Navbar />
+            <Map />
+        </>
+    );
+  }
+  
+  export default Main;
